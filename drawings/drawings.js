7a06890b10832624c5e3d4f1148ec7a72d2ca521
LyraHunt <lyrawhunt@gmail.com>
2025-08-13 23:25:23 -0400

    Adding a hover effect to the drawings and filling in the drawing gaps with blanks

--- drawings/drawings.js
+++ drawings/drawings.js
@@ -19,6 +19,8 @@ function reloadDrawingScales() {
     var drawingElements = drawingsContainer.children
     var currentWindowWidth = window.innerWidth
 
+    removeGaps();
+
     for (let i = 0; i < drawingElements.length; i++) {
         var currentDrawingElement = drawingElements[i]
         var currentDrawingData = drawings[Number(currentDrawingElement.id)]
@@ -31,6 +33,41 @@ function reloadDrawingScales() {
         currentDrawingElement.style.gridColumnEnd = "span " + currentDrawingSize.x
         currentDrawingElement.style.gridRowEnd = "span " + currentDrawingSize.y
     }
+
+    fillInGaps();
+}
+
+function removeGaps() {
+    var drawingElements = drawingsContainer.children
+    var startingLength = drawingElements.length;
+    for (let i = 0; i < startingLength; i++) {
+        var currentDrawingElement = drawingElements[startingLength - i - 1];
+        if (currentDrawingElement.classList.contains("blankDrawing")) {
+            console.log("remove")
+            currentDrawingElement.remove()
+        }
+    }
+}
+
+function fillInGaps() {
+    var isFilled = false
+    for (let i = 0; i < 100; i++) {
+        var previousHeight = drawingsContainer.offsetHeight
+        if (!isFilled) {
+            var newDrawing = document.createElement("div");
+            newDrawing.classList.add("drawing");
+            newDrawing.classList.add("blankDrawing");
+            drawingsContainer.appendChild(newDrawing);
+
+            var newDrawingImg = document.createElement("img");
+            newDrawingImg.src = "images/blank.png";
+            newDrawing.appendChild(newDrawingImg);
+        }
+        if (drawingsContainer.offsetHeight > previousHeight) {
+            isFilled = true;
+            newDrawing.remove()
+        }
+    }
 }
 
 generateDrawings();
@@ -38,9 +75,21 @@ generateDrawings();
 generateDrawings();
 generateDrawings();
 reloadDrawingScales();
+fillInGaps();
 
 var mobileWindow = window.matchMedia("(max-width: 768px)");
 
 mobileWindow.addEventListener("change", function() {
     reloadDrawingScales();
 });
+
+var lastDrawingContainerHeight = 0
+
+new ResizeObserver(function() {
+    console.log("heen");
+    if (lastDrawingContainerHeight !== drawingsContainer.offsetHeight) {
+        removeGaps();
+        fillInGaps();
+    }
+    lastDrawingContainerHeight = drawingsContainer.offsetHeight;
+}).observe(drawingsContainer);
